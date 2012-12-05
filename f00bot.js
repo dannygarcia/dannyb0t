d149d9ab03daa19f428f8d0e141cc8538855be64
Richard Herrera <rich@doctyper.com>
2012-12-04 16:56:10 -0800

    YouTube IDs also contain -'s and _'s

--- f00bot.js
+++ f00bot.js
@@ -497,7 +497,7 @@ f00bert.prototype.checkForTweet = function (context, text) {
 };
 
 f00bert.prototype.checkForYouTube = function (context, text) {
-	var regExp = /youtube.com\/(?:.*)v=([a-zA-Z0-9]+)/;
+	var regExp = /youtube.com\/(?:.*)v=([a-zA-Z0-9_-]+)/;
 	var match = text.match(regExp);
 
 	if (match && match.length) {
