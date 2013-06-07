8182ac9501a67df87c009111aa6aa328c4c48a08
Richard Herrera <rich@doctyper.com>
2013-06-07 12:54:30 -0700

    Move sensitive data to profile.js

--- src/listeners/askCleverbot.js
+++ src/listeners/askCleverbot.js
@@ -8,9 +8,25 @@ module.exports = function (f00bot, profile) {
 		hellban: true,
 		killjoy: true
 	}, function (context, text) {
-		if ((/assemble|reboot|cycle/).test(text) && (context.sender.name === "doctyper" || context.sender.name === "landon")) {
-			context.channel.echo("Got it.");
-			cp.exec("curl -I http://localhost:9876/update", function (err, stdout, stderr) {
+		if (!profile.updaters || !profile.update) {
+			return;
+		}
+
+		if ((/assemble|reboot|cycle/).test(text) && profile.updaters.indexOf(context.sender.name) !== -1) {
+			var confirm = [
+				"Got it.",
+				"Yep.",
+				"Sure.",
+				"*sigh*",
+				"Fine.",
+				"Ok.",
+				"Yes ma'am."
+			];
+
+			var rand = Math.floor(Math.random() * confirm.length);
+			context.channel.echo(confirm[rand]);
+
+			cp.exec("curl -I " + profile.update, function (err, stdout, stderr) {
 				if (stdout) {
 					console.log(stdout);
 				}
